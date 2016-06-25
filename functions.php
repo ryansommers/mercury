5c07c62119107211966e65d406166bfc051a1a1c
Kyle Scheuerlein <kscheuerlein@icloud.com>
2016-06-24 19:55:43 -0700

    Add support for Jetpack Infinite Scroll

--- functions.php
+++ functions.php
@@ -14,4 +14,22 @@ function mercury_load_fonts() {
   wp_enqueue_style( 'googleFonts');
 }
 
+// Add Support for Jetpack Infinite Scroll
+function infinite_scroll_get_posts() {
+	while ( have_posts() ) : the_post();
+		p2_load_entry();
+	endwhile;
+}
+function p2_infinite_scroll_init() {
+	add_theme_support( 'infinite-scroll', array(
+		'type' => 'click',
+		'container' => 'postlist',
+		'wrapper' => false,
+		'render' => infinite_scroll_get_posts,
+		'posts_per_page' => 5,
+		'footer' => 'page',
+	) );
+}
+add_action( 'after_setup_theme', 'p2_infinite_scroll_init' );
+
 ?>
